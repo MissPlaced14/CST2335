44be5340ef9b96bcbc027afd1253a6e721d07997
Patty <pmrm1984@gmail.com>
2018-09-26 13:15:49 -0400

    Added 3 activities for lab 2

---
+++ src/main/java/com/example/patty/andoridlabs/LoginActivitiy.java
@@ -0,0 +1,13 @@
+package com.example.patty.andoridlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivitiy extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activitiy);
+    }
+}
